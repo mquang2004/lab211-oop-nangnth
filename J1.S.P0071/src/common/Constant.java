635e85e64a2b0fdfb6620a3d493705f6c5dfdeb7
quangle212 <lmq0212@gmail.com>
2026-06-27 22:37:11 +0700

    create: EBank

--- J1.S.P0071/src/common/Constant.java
+++ J1.S.P0071/src/common/Constant.java
@@ -8,16 +8,4 @@ public class Constant {
     public static final String REG_DATE = "\\d{2}-\\d{2}-\\d{4}";
     public static final String REG_PLAN_TIME = "([8-9]|1[0-7])(\\.0|\\.5)?";
     public static final String REG_TEXT = ".+";
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public static final String REG_TIME = "([8-9]|1[0-7])(\\.0|\\.5)?";
 }
